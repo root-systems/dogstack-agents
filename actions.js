aedf5d938f9428f326c4f2b9def8ee6e0f121b4b
Gregory Kan <kan.gregory@gmail.com>
2017-07-28 16:39:37 +1200

    fix(relationships): export relationship actions

--- actions.js
+++ actions.js
@@ -2,3 +2,4 @@ exports.agents = require('./agents').actions
 exports.credentials = require('./credentials').actions
 exports.profiles = require('./profiles').actions
 exports.authentication = require('./authentication').actions
+exports.relationships = require('./relationships').actions
