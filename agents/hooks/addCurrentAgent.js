f2f80bb720a6d72194903510049418331bfa2780
Michael Smith <michael@msmith.nz>
2017-09-11 14:50:05 +0100

    assume an agent can only have one profile

--- agents/hooks/addCurrentAgent.js
+++ agents/hooks/addCurrentAgent.js
@@ -1,9 +1,16 @@
 const merge = require('ramda/src/merge')
 const isNil = require('ramda/src/isNil')
 const path = require('ramda/src/path')
+const pipe = require('ramda/src/pipe')
+const not = require('ramda/src/not')
+const hasAgentId = pipe(
+  path(['params', 'credential', 'agentId']),
+  isNil,
+  not
+)
 
 module.exports = function addCurrentAgent (hook) {
-  if (isNil(path(['params', 'credential', 'agentId'], hook))) return hook
+  if (hasAgentId(hook)) return hook
 
   const agents = hook.app.service('agents')
 
--- profiles/hooks/addCurrentProfile.js
+++ profiles/hooks/addCurrentProfile.js
@@ -1,17 +1,28 @@
 const merge = require('ramda/src/merge')
 const isNil = require('ramda/src/isNil')
 const path = require('ramda/src/path')
-
+const pipe = require('ramda/src/pipe')
+const not = require('ramda/src/not')
+const hasAgentId = pipe(
+  path(['params', 'credential', 'agentId']),
+  isNil,
+  not
+)
 module.exports = function addCurrentProfile (hook) {
-  if (isNil(path(['params', 'credential', 'agentId'], hook))) return hook
+  if (hasAgentId(hook)) return hook
 
   const profiles = hook.app.service('profiles')
 
-  return profiles.find({ query: { agentId: hook.params.credential.agentId } })
-    .then((profiles) => {
-      hook.params = merge(hook.params, {
-        profiles: profiles
-      })
-      return hook
+  return profiles.find({
+    query: {
+      agentId: hook.params.credential.agentId,
+      $limit: 1
+    }
+  })
+  .then((profile) => {
+    hook.params = merge(hook.params, {
+      profile: profile
     })
+    return hook
+  })
 }
