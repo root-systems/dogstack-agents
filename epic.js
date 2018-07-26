ac423ef022316f77c55a081fb882e99171de608e
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-07-26 16:05:29 +1200

    WIP: typeError when running with dogstack-example, invalid object instead of stream

--- epic.js
+++ epic.js
@@ -1,15 +1,15 @@
 const { combineEpics } = require('redux-observable')
 
 const agents = require('./agents').epic
-const credentials = require('./credentials').epic
-const profiles = require('./profiles').epic
+// const credentials = require('./credentials').epic
+// const profiles = require('./profiles').epic
 const authentication = require('./authentication').epic
 const relationships = require('./relationships').epic
 
 module.exports = combineEpics(
   agents,
-  credentials,
-  profiles,
+  // credentials,
+  // profiles,
   authentication,
   relationships
 )
--- updater.js
+++ updater.js
@@ -1,15 +1,15 @@
 const { concat } = require('redux-fp')
 
 const agents = require('./agents').updater
-const credentials = require('./credentials').updater
-const profiles = require('./profiles').updater
+// const credentials = require('./credentials').updater
+// const profiles = require('./profiles').updater
 const authentication = require('./authentication').updater
 const relationships = require('./relationships').updater
 
 module.exports = concat(
   agents,
-  credentials,
-  profiles,
+  // credentials,
+  // profiles,
   authentication,
   relationships
 )
